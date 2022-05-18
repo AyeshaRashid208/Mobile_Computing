e33855adfffed28312ca0938843e8ebcb987e2d7
bsef19m531 <rashidayesha208@gmail.com>
2022-05-18 13:38:43 +0500

    Commit Intent_practice3

--- Intents_Practice3/app/src/main/java/com/example/intents_practice3/MainActivity.java
+++ Intents_Practice3/app/src/main/java/com/example/intents_practice3/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.intents_practice3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
+
 }
